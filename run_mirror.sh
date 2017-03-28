d08f57ccf92d0ee65f0e36b5eae12464e52a54f4
olegje31 <oly.jonny@gmail.com>
2017-03-28 21:32:16 +0200

    Added the run_mirror.sh

---
+++ run_mirror.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git pull
+#python3 /home/pi/Oles\ Smart\ Mirror/Oles\ Smart\ Mirror\ V1.18_pi_copy.py
+#This is a "fake" file. the real one that starts the program on boot os in /home/pi/run_mirror.sh
