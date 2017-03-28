49377ff7be6474f546a046a9020f332ce9cff907
olegje31 <oly.jonny@gmail.com>
2017-03-28 21:40:43 +0200

    Fixed line in run_mirror.sh

--- run_mirror.sh
+++ run_mirror.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git pull
-#python3 /home/pi/Oles\ Smart\ Mirror/Oles\ Smart\ Mirror\ V1.18_pi_copy.py
+python3 /home/pi/OlesSmartMirror/Oles\ Smart\ Mirror\ V1.20_pi.py
 #This is a "fake" file. the real one that starts the program on boot os in /home/pi/run_mirror.sh
+#This will soon be fixed
