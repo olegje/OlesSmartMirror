f65377051def4f958c7f751dbc8cdd6e9ae64c85
olegje31 <oly.jonny@gmail.com>
2017-04-02 16:45:08 +0200

    bug fix

--- run_mirror.sh
+++ run_mirror.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 git pull
 python3 /home/pi/OlesSmartMirror/Oles\ Smart\ Mirror\ V1.20_pi.py
-#This is a "fake" file. the real one that starts the program on boot os in /home/pi/run_mirror.sh
-#This will soon be fixed
