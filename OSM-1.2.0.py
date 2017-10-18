e214a1af91322417a30e2e4bfea9548648ce329a
olegje31 <oly.jonny@gmail.com>
2017-10-18 19:18:15 +0200

    fixed timing

--- OSM-1.2.0.py
+++ OSM-1.2.0.py
@@ -363,7 +363,7 @@ class Tempratures(tk.Frame):
         self.out_temp.after(60000, self.update)
     def DB_communication(self):
         DBHandle.insert_to_DB()
-        self.out_temp.after(10000, self.DB_communication)
+        self.out_temp.after(300000, self.DB_communication)
         
 
    
