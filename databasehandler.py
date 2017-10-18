9d9668a8e2bdf26bbd740921f23fabf9447f8bbd
olegje31 <oly.jonny@gmail.com>
2017-10-18 19:23:19 +0200

    code fix

--- databasehandler.py
+++ databasehandler.py
@@ -103,13 +103,13 @@ class Tempratures():
     def retrive_out_temp(self):
         try:
             cursor = self.cnx.cursor()
-            query = ("SELECT * FROM `Outdoor` ORDER BY `Time` DESC LIMIT 200")# 288 times 5 minutes between inserts = 24 hours
+            query = ("SELECT * FROM `Outdoor` ORDER BY `Time` DESC LIMIT 288")# 288 times 5 minutes between inserts = 24 hours
             self.out_temp_history = cursor.execute(query)
             logger.debug("Out tempratures retrived")
             print(self.out_temp_history)
             cursor.close()
-        except:
-            logger.error("Error!")    
+        #except:
+            #logger.error("Error!")    
 
 if __name__ == '__main__':
     logger.info('Script started as main')
